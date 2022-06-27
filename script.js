97ae9d15e81eaa72c3e10b7885c5cd23ae9d758b
esrig <joshua.esrig@gmail.com>
2022-06-26 21:19:19 -0700

    createRows and createCols methods are complete, non natural number inputs still start game (bug)

--- script.js
+++ script.js
@@ -3,13 +3,33 @@ const container = document.querySelector("#container");
 function createRows(numRows) {
     for(let i = 0; i < numRows; i++) {
         let tempRow = document.createElement("div");
-        tempRow.className = "box";
+        tempRow.className = "row";
         rowArr.push(tempRow);
         container.appendChild(tempRow);
     }
 
 }
-
 function createCols(numCols) {
+    rowArr = document.querySelectorAll(".row");
+    rowArr.forEach((row) => {
+        for(let i = 0; i < numCols; i++) {
+        let tempBox = document.createElement("div");
+        tempBox.className = "box";
+        row.appendChild(tempBox);
+    }
+    });
+
+    
 
 }
+function startGame() {
+    container.innerHtml = "";
+    const dimension = prompt("Enter a natural number n to create an n * n grid");
+    while(typeof dimension != "number" && dimension <= 0) {
+        dimension = prompt("Your input was not a natural number (an integer greater than 0). Please enter a natural number.");
+        console.log("a");
+    }
+    createRows(dimension); createCols(dimension); 
+}
+
+startGame();
